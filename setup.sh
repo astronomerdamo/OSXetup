e172bd8de443e1617ac8a17a7fb38bbf398c058b
Damien Robertson <robertsondamien@gmail.com>
2014-10-18 13:02:27 -0400

    Added Python install based on user choice

--- setup.sh
+++ setup.sh
@@ -19,3 +19,26 @@ brew cask install google-chrome firefox iterm2 onyx atom brackets dropbox flux v
 
 #brew cask install mactex
 # Note this installs the full 2.2 GB mactex package. I haven't decided if brew-cask is needed for mactex, might make more sense to just manually d/l from <https://tug.org/mactex/>
+
+# Python install prompt
+echo -n "Do you want to install Python? [y/n] > "
+read pyAnswer
+
+if [ "$pyAnswer" == "y" ]; then
+    echo "Note all python installations are Python 3"
+    echo -n "Source: 1 - homebrew Python, 2 - Anaconda Python (Continuum Analytics) [1/2] > "
+    read pyChoice
+    
+    if [ "$pyChoice" == 1 ]; then
+        echo "Installing Python via homebrew"
+        brew install python3
+        pip3 install --upgrade pip3
+        pip3 install setuptools
+    else
+        wget -O ~/Downloads/Miniconda3-3.7.0-MacOSX-x86_64.sh http://repo.continuum.io/miniconda/Miniconda3-3.7.0-MacOSX-x86_64.sh
+        bash ~/Downloads/Miniconda3-3.7.0-MacOSX-x86_64.sh
+    fi
+
+else
+    echo "Okay, no Python but you're missing out"
+fi
