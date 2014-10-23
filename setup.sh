babbf9982bc286404f0cae06701d50a74a916500
Damien Robertson <robertsondamien@gmail.com>
2014-10-23 14:30:10 -0400

    Improved script verbosity, added app linking step

--- setup.sh
+++ setup.sh
@@ -4,25 +4,38 @@
 
 # Installing xcode command line tools - May Requires user interaction
 sudo xcode-select --install
-
-# Install homebrew for OS X
-ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-brew doctor
+echo "OSXetup > Press any key when install completes"
+read tmp
 # You might need to open xcode and 'agree' to the terms of service before use?
 
-# Install brew programs
-brew install wget git vim macvim node fish
+echo ""
+echo "OSXetup > Install homebrew"
+echo ""
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew doctor
 
-# Tap in install brew-cask along with binary apps
+echo ""
+echo "OSXetup > Install homebrew programs"
+echo ""
+brew install wget git vim macvim node fish
+brew linkapps
+
+echo ""
+echo "OSXetup > Install homebrew cask programs"
+echo ""
 brew tap caskroom/cask
 brew install brew-cask
-brew cask install google-chrome firefox iterm2 onyx atom brackets dropbox flux vlc xquartz
+brew cask install google-chrome firefox iterm2 onyx atom brackets dropbox flux vlc xquartz steam
 
+#echo ""
+#echo "OSXetup > Install MacTex via homebrew"
+#echo ""
 #brew cask install mactex
-# Note this installs the full 2.2 GB mactex package. I haven't decided if brew-cask is needed for mactex, might make more sense to just manually d/l from <https://tug.org/mactex/>
+#Note this installs the full 2.2 GB mactex package. I haven't decided if brew-cask is needed for mactex, might make more sense to just manually d/l from <https://tug.org/mactex/>
 
 # Python install prompt
-echo -n "Do you want to install Python? [y/n] > "
+echo ""
+echo -n "OSXetup > Do you want to install Python? [y/n] > "
 read pyAnswer
 case $pyAnswer in
     [yY])
@@ -45,7 +58,7 @@ case $pyAnswer in
                 rm ~/Downloads/Miniconda3-3.7.0-MacOSX-x86_64.sh
                 ;;
             *) 
-                echo "Not a valid option"
+                echo "Not a valid option - skipping install"
                 ;;
         esac    
         ;;
