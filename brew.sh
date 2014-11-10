c7e6133d06a304e4d3ce8393ce6efe304ad7ec1f
Andrew Valencik <andrew.valencik@gmail.com>
2014-11-09 20:36:16 -0400

    Cleaned up brews and casks.
    Removed things we can live without, and things that should be in personal
    manifest files.

--- brew.sh
+++ brew.sh
@@ -7,20 +7,15 @@ brew upgrade
 echo ""
 echo "OSXetup > Install homebrew programs"
 echo ""
-brew install wget
 brew install git
 brew install vim --with-python3
+brew install wget
 brew install node
-brew install fish
+brew tap caskroom/cask
+brew install brew-cask
 
 #Link applications
 brew linkapps
-
-echo ""
-echo "OSXetup > Install homebrew cask"
-echo ""
-brew tap caskroom/cask
-brew install brew-cask
 
 #Remove old versions from the hombrew cellar
 brew cleanup
--- cask.sh
+++ cask.sh
@@ -6,17 +6,13 @@ brew cask update
 echo ""
 echo "OSXetup > Install homebrew cask programs"
 echo ""
-brew cask install google-chrome
 brew cask install firefox
+brew cask install google-chrome
 brew cask install iterm2
-brew cask install onyx
-brew cask install atom
-brew cask install brackets
 brew cask install dropbox
 brew cask install flux
 brew cask install vlc
 brew cask install xquartz
-brew cask install steam
 
 #Remove old versions from the hombrew cellar
 brew cask cleanup
